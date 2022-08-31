0bf3c258aa640c2daf71a055a29fedd6ab6da382
atzuur <johannes.pihko@gmail.com>
2022-09-01 01:33:29 +0300

    create c files for each header

--- src/helpers.h
+++ src/helpers.h
@@ -1,41 +1,10 @@
+#pragma once
+
 #include <windows.h>
 #include <psapi.h>
 
-int AutoDelay()
-{
-    SYSTEM_INFO sysinfo;
-    GetSystemInfo(&sysinfo);
-    int cpu_count = sysinfo.dwNumberOfProcessors;
-    if (cpu_count <= 4)
-    {
-        return 1000;
-    }
-    else
-    {
-        return 100;
-    }
-}
+int AutoDelay();
 
-char *GetTitle(HWND hwnd)
-{
-    int len = GetWindowTextLengthA(hwnd) + 1;
-    char *buf = malloc((sizeof(char) * len));
-    GetWindowTextA(hwnd, buf, len);
-    return buf;
-}
-
-char *GetExe(DWORD pid)
-{
-    HANDLE hProc = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, FALSE, pid);
-    char *buf = malloc((sizeof(char) * MAX_PATH) + 1);
-    GetModuleFileNameExA(hProc, NULL, buf, MAX_PATH);
-    CloseHandle(hProc);
-    return buf;
-}
-
-DWORD GetPID(HWND hwnd)
-{
-    DWORD pid;
-    GetWindowThreadProcessId(hwnd, &pid);
-    return pid;
-}
+char *GetTitle(HWND hwnd);
+char *GetExe(DWORD pid);
+DWORD GetPID(HWND hwnd);
--- src/main.c
+++ src/main.c
@@ -1,8 +1,9 @@
 #include "funcs.h"
+#include "helpers.h"
 #include <stdio.h>
 
 int main(int argc, char *argv[])
-{  
+{
     chdir(dirname(argv[0]));
     OptsExist();
     ResApply();
